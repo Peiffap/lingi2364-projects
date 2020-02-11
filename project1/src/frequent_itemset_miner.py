2a4a2ec26bd6137fb7aabf0fc3ec69b40d79a901
Gilles Peiffer <gilles.peiffer@outlook.com>
2020-02-11 18:28:51 +0100

    [project1] Implement naive apriori

--- project1/src/frequent_itemset_miner.py
+++ project1/src/frequent_itemset_miner.py
@@ -18,49 +18,92 @@ any order.
 
 Do not change the signature of the apriori and alternative_miner methods as they will be called by the test script.
 
-__authors__ = "<write here your group, first name(s) and last name(s)>"
+__authors__ = Group 13, Gilles Peiffer & Liliya Semerikova
 """
+
+import itertools
 
 
 class Dataset:
-	"""Utility class to manage a dataset stored in a external file."""
+    """Utility class to manage a dataset stored in a external file."""
 
-	def __init__(self, filepath):
-		"""reads the dataset file and initializes files"""
-		self.transactions = list()
-		self.items = set()
+    def __init__(self, filepath):
+        """reads the dataset file and initializes files"""
+        self.transactions = list()
+        self.items = set()
 
-		try:
-			lines = [line.strip() for line in open(filepath, "r")]
-			lines = [line for line in lines if line]  # Skipping blank lines
-			for line in lines:
-				transaction = list(map(int, line.split(" ")))
-				self.transactions.append(transaction)
-				for item in transaction:
-					self.items.add(item)
-		except IOError as e:
-			print("Unable to read dataset file!\n" + e)
+        try:
+            lines = [line.strip() for line in open(filepath, "r")]
+            lines = [line for line in lines if line]  # Skipping blank lines
+            for line in lines:
+                transaction = list(map(int, line.split(" ")))
+                self.transactions.append(transaction)
+                for item in transaction:
+                    self.items.add(item)
+        except IOError as e:
+            print("Unable to read dataset file!\n" + e)
 
-	def trans_num(self):
-		"""Returns the number of transactions in the dataset"""
-		return len(self.transactions)
+    def trans_num(self):
+        """Returns the number of transactions in the dataset"""
+        return len(self.transactions)
 
-	def items_num(self):
-		"""Returns the number of different items in the dataset"""
-		return len(self.items)
+    def items_num(self):
+        """Returns the number of different items in the dataset"""
+        return len(self.items)
 
-	def get_transaction(self, i):
-		"""Returns the transaction at index i as an int array"""
-		return self.transactions[i]
+    def get_transaction(self, i):
+        """Returns the transaction at index i as an int array"""
+        return self.transactions[i]
+
+    def covers(self, i, itemset):
+        """Returns whether the given itemset covers transaction i"""
+        return itemset.issubset(self.get_transaction(i))
+
+    def support(self, itemset):
+        """Returns the support of the given itemset for the current list of transactions"""
+        s = 0
+        for i in range(self.trans_num()):
+            if self.covers(i, itemset):
+                s += 1
+        return s
+
+    def frequency(self, itemset):
+        """Returns the frequency of the given itemset for the current list of transactions"""
+        return self.support(itemset)/self.trans_num()
 
 
 def apriori(filepath, minFrequency):
-	"""Runs the apriori algorithm on the specified file with the given minimum frequency"""
-	# TODO: implementation of the apriori algorithm
-	print("Not implemented")
+    """Runs the apriori algorithm on the specified file with the given minimum frequency"""
+    data = Dataset(filepath)
+    minSupport = minFrequency * data.trans_num()
 
+    level = 1
+    F = [[set([])], [set([])]]
+    while True:
+        F.append([])
+        Clist = [set(i) for i in itertools.combinations(range(1, data.items_num() + 1), level)]
+        for C in Clist:
+            subsets = [set(i) for i in itertools.combinations(C, len(C) - 1)]
+            allIn = True
+            for s in subsets:
+                if s not in F[level-1]:
+                    allIn = False
+                    break
+            if allIn and data.support(C) >= minSupport:
+                F[level].append(C)
+        if (len(F[level]) != 0):
+            for s in F[level]:
+                if s != set([]):
+                    print(list(s), " ({:g})".format(data.frequency(s)))
+        else:
+            break
+
+        level += 1
 
 def alternative_miner(filepath, minFrequency):
-	"""Runs the alternative frequent itemset mining algorithm on the specified file with the given minimum frequency"""
-	# TODO: either second implementation of the apriori algorithm or implementation of the depth first search algorithm
-	print("Not implemented")
+    """Runs the alternative frequent itemset mining algorithm on the specified file with the given minimum frequency"""
+    # TODO: either second implementation of the apriori algorithm or implementation of the depth first search algorithm
+    print("Not implemented")
+
+if __name__== "__main__":
+    apriori("../statement/Datasets/toy.dat", 0.125)
