e3693aab7265ab97fdb80077bf982e1432e37217
Gilles Peiffer <gilles.peiffer@outlook.com>
2020-04-14 03:55:10 +0200

    Project 2: Rewrite benchmarking code to be more modular

--- project2/src/benchmark_size.py
+++ project2/src/benchmark_size.py
@@ -6,284 +6,106 @@ Benchmarks for the various frequent pattern mining algorithms.
 from timeout import timeout
 import pickle
 
-import resource
-import time
+import tracemalloc
+from queue import Queue, Empty
+from resource import getrusage, RUSAGE_SELF
+from threading import Thread
+from time import sleep
 
-from stoppable_thread import StoppableThread
+def memory_monitor(command_queue: Queue, d, poll_interval=1):
+    tracemalloc.start()
+    old_max = 0
+    snapshot = None
+    while True:
+        try:
+            command_queue.get(timeout=poll_interval)
+            if snapshot is not None:
+                snapshot = snapshot.filter_traces((
+                    tracemalloc.Filter(False, "<frozen importlib._bootstrap>"),
+                    tracemalloc.Filter(False, "<unknown>"),
+                ))
+                top_stats = snapshot.statistics('lineno')
+                total = sum(stat.size for stat in top_stats)
+                d.append(total / 1024) # KiB
 
-
-class MyLibrarySniffingClass(StoppableThread):
-    def __init__(self, target_lib_call, pf, nf, k, verbose):
-        super(MyLibrarySniffingClass, self).__init__()
-        self.target_function = target_lib_call
-        self.pf = pf
-        self.nf = nf
-        self.k = k
-        self.verbose = verbose
-
-    def startup(self):
-        pass
-
-    def cleanup(self):
-        # Overload the cleanup function
-        pass
-
-    def mainloop(self):
-        # Start the library Call
-        self.target_function(self.pf, self.nf, self.k, self.verbose)
-
-        # Kill the thread when complete
-        self.stop()
+            return
+        except Empty:
+            max_rss = getrusage(RUSAGE_SELF).ru_maxrss
+            if max_rss > old_max:
+                old_max = max_rss
+                snapshot = tracemalloc.take_snapshot()
 
 import matplotlib.pyplot as plt
 
 plt.style.use("ggplot")
 
+def aux(algs, names, file):
+
+    datasets = ['Protein', 'Reuters']
+    pos_files = ['SRC1521', 'earn']
+    neg_files = ['PKA_group15', 'acq']
+
+    sizes = {}
+    for data in datasets:
+        sizes[data] = {}
+        for alg in names:
+            sizes[data][alg] = []
+
+    for j, data in enumerate(datasets):
+        for i in range(len(algs)):
+            print()
+            alg = algs[i]
+            k = 1
+            while True:
+                try:
+                    print("data: %s, alg: %s, k: %g" % (data, names[i], k))
+                    with timeout(seconds=200):
+                        queue = Queue()
+                        poll_interval = 0.01
+                        monitor_thread = Thread(target=memory_monitor, args=(queue, sizes[data][names[i]], poll_interval))
+                        monitor_thread.start()
+                        try:
+                            sleep(2) # Start up time.
+                            alg("%s/%s.txt" % (data, pos_files[j]), "%s/%s.txt" % (data, neg_files[j]), k, False)
+                            sleep(3) # Shut down time.
+                        finally:
+                            queue.put('stop')
+                            monitor_thread.join()
+                    k *= 2
+                except (TimeoutError, RecursionError) as e:
+                    print(e)
+                    break
+
+    pickle.dump(sizes, open("../report/data/sizes_%s.p" % (file), "wb"))
+
 if __name__ == "__main__":
-
-    memory_usage_refresh = .005 # Seconds
-
     import sumsup.PrefixSpan_sumsup_heap as ps_heap
     import sumsup.PrefixSpan_sumsup_sorted_list as ps_sl
     import sumsup.SPADE as spade
     algs = [file.main for file in [ps_heap, ps_sl, spade]]
     names = ['PrefixSpan_heap', 'PrefixSpan_sorted_list', 'SPADE']
-    datasets = ['Protein', 'Reuters']
-    pos_files = ['SRC1521', 'earn']
-    neg_files = ['PKA_group15', 'acq']
-
-    sizes = {}
-    for data in datasets:
-        sizes[data] = {}
-        for alg in names:
-            sizes[data][alg] = []
-
-    for j, data in enumerate(datasets):
-        for i in range(len(algs)):
-            print()
-            alg = algs[i]
-            k = 1
-            while True:
-                try:
-                    print("data: %s, alg: %s, k: %g" % (data, names[i], k))
-                    max_memory = 0
-                    with timeout(seconds=200):
-                        # Lib Testing Code
-                        mythread = MyLibrarySniffingClass(alg, "%s/%s.txt" % (data, pos_files[j]), "%s/%s.txt" % (data, neg_files[j]), k, False)
-                        mythread.start()
-
-                        start_mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-                        delta_mem = 0
-
-                        while(1):
-                            time.sleep(memory_usage_refresh)
-                            delta_mem = (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss) - start_mem
-                            if delta_mem > max_memory:
-                                max_memory = delta_mem
-
-                            # Check to see if the library call is complete
-                            if mythread.isShutdown():
-                                break
-                    sizes[data][names[i]].append(max_memory)
-                    k *= 2
-                except TimeoutError as e:
-                    print(e)
-                    break
-
-    pickle.dump(sizes, open("../report/data/sizes_sumsup.p", "wb"))
-
+    aux(algs, names, 'sumsup')
 
     import wracc.PrefixSpan_wracc_heap as ps_heap
     import wracc.PrefixSpan_wracc_sorted_list as ps_sl
     algs = [file.main for file in [ps_heap, ps_sl]]
     names = ['PrefixSpan_heap', 'PrefixSpan_sorted_list']
-    datasets = ['Protein', 'Reuters']
-    pos_files = ['SRC1521', 'earn']
-    neg_files = ['PKA_group15', 'acq']
-
-    sizes = {}
-    for data in datasets:
-        sizes[data] = {}
-        for alg in names:
-            sizes[data][alg] = []
-
-    for j, data in enumerate(datasets):
-        for i in range(len(algs)):
-            print()
-            alg = algs[i]
-            k = 1
-            while True:
-                try:
-                    print("data: %s, alg: %s, k: %g" % (data, names[i], k))
-                    max_memory = 0
-                    with timeout(seconds=200):
-                        # Lib Testing Code
-                        mythread = MyLibrarySniffingClass(alg, "%s/%s.txt" % (data, pos_files[j]), "%s/%s.txt" % (data, neg_files[j]), k, False)
-                        mythread.start()
-
-                        start_mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-                        delta_mem = 0
-
-                        while(1):
-                            time.sleep(memory_usage_refresh)
-                            delta_mem = (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss) - start_mem
-                            if delta_mem > max_memory:
-                                max_memory = delta_mem
-
-                            # Check to see if the library call is complete
-                            if mythread.isShutdown():
-                                break
-                    sizes[data][names[i]].append(max_memory)
-                    k *= 2
-                except TimeoutError as e:
-                    print(e)
-                    break
-
-    pickle.dump(sizes, open("../report/data/sizes_wracc.p", "wb"))
-
+    aux(algs, names, 'wracc')
 
     import closed_wracc.CloSpan_wracc_heap as cs_heap
     import closed_wracc.CloSpan_wracc_sorted_list as cs_sl
     algs = [file.main for file in [cs_heap, cs_sl]]
     names = ['CloSpan_heap', 'CloSpan_sorted_list']
-    datasets = ['Protein', 'Reuters']
-    pos_files = ['SRC1521', 'earn']
-    neg_files = ['PKA_group15', 'acq']
-
-    sizes = {}
-    for data in datasets:
-        sizes[data] = {}
-        for alg in names:
-            sizes[data][alg] = []
-
-    for j, data in enumerate(datasets):
-        for i in range(len(algs)):
-            print()
-            alg = algs[i]
-            k = 1
-            while True:
-                try:
-                    print("data: %s, alg: %s, k: %g" % (data, names[i], k))
-                    max_memory = 0
-                    with timeout(seconds=200):
-                        # Lib Testing Code
-                        mythread = MyLibrarySniffingClass(alg, "%s/%s.txt" % (data, pos_files[j]), "%s/%s.txt" % (data, neg_files[j]), k, False)
-                        mythread.start()
-
-                        start_mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-                        delta_mem = 0
-
-                        while(1):
-                            time.sleep(memory_usage_refresh)
-                            delta_mem = (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss) - start_mem
-                            if delta_mem > max_memory:
-                                max_memory = delta_mem
-
-                            # Check to see if the library call is complete
-                            if mythread.isShutdown():
-                                break
-                    sizes[data][names[i]].append(max_memory)
-                    k *= 2
-                except TimeoutError as e:
-                    print(e)
-                    break
-
-    pickle.dump(sizes, open("../report/data/sizes_closed_wracc.p", "wb"))
-
+    aux(algs, names, 'closed_wracc')
 
     import closed_abswracc.CloSpan_abswracc_heap as cs_heap
     import closed_abswracc.CloSpan_abswracc_sorted_list as cs_sl
     algs = [file.main for file in [cs_heap, cs_sl]]
     names = ['CloSpan_heap', 'CloSpan_sorted_list']
-    datasets = ['Protein', 'Reuters']
-    pos_files = ['SRC1521', 'earn']
-    neg_files = ['PKA_group15', 'acq']
-
-    sizes = {}
-    for data in datasets:
-        sizes[data] = {}
-        for alg in names:
-            sizes[data][alg] = []
-
-    for j, data in enumerate(datasets):
-        for i in range(len(algs)):
-            print()
-            alg = algs[i]
-            k = 1
-            while True:
-                try:
-                    print("data: %s, alg: %s, k: %g" % (data, names[i], k))
-                    max_memory = 0
-                    with timeout(seconds=200):
-                        # Lib Testing Code
-                        mythread = MyLibrarySniffingClass(alg, "%s/%s.txt" % (data, pos_files[j]), "%s/%s.txt" % (data, neg_files[j]), k, False)
-                        mythread.start()
-
-                        start_mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-                        delta_mem = 0
-
-                        while(1):
-                            time.sleep(memory_usage_refresh)
-                            delta_mem = (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss) - start_mem
-                            if delta_mem > max_memory:
-                                max_memory = delta_mem
-
-                            # Check to see if the library call is complete
-                            if mythread.isShutdown():
-                                break
-                    sizes[data][names[i]].append(max_memory)
-                    k *= 2
-                except TimeoutError as e:
-                    print(e)
-                    break
-
-    pickle.dump(sizes, open("../report/data/sizes_closed_abswracc.p", "wb"))
-
+    aux(algs, names, 'closed_abswracc')
 
     import closed_infogain.CloSpan_infogain_heap as cs_heap
     import closed_infogain.CloSpan_infogain_sorted_list as cs_sl
     algs = [file.main for file in [cs_heap, cs_sl]]
     names = ['CloSpan_heap', 'CloSpan_sorted_list']
-    datasets = ['Protein', 'Reuters']
-    pos_files = ['SRC1521', 'earn']
-    neg_files = ['PKA_group15', 'acq']
-
-    sizes = {}
-    for data in datasets:
-        sizes[data] = {}
-        for alg in names:
-            sizes[data][alg] = []
-
-    for j, data in enumerate(datasets):
-        for i in range(len(algs)):
-            print()
-            alg = algs[i]
-            k = 1
-            while True:
-                try:
-                    print("data: %s, alg: %s, k: %g" % (data, names[i], k))
-                    max_memory = 0
-                    with timeout(seconds=200):
-                        # Lib Testing Code
-                        mythread = MyLibrarySniffingClass(alg, "%s/%s.txt" % (data, pos_files[j]), "%s/%s.txt" % (data, neg_files[j]), k, False)
-                        mythread.start()
-
-                        start_mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-                        delta_mem = 0
-
-                        while(1):
-                            time.sleep(memory_usage_refresh)
-                            delta_mem = (resource.getrusage(resource.RUSAGE_SELF).ru_maxrss) - start_mem
-                            if delta_mem > max_memory:
-                                max_memory = delta_mem
-
-                            # Check to see if the library call is complete
-                            if mythread.isShutdown():
-                                break
-                    sizes[data][names[i]].append(max_memory)
-                    k *= 2
-                except TimeoutError as e:
-                    print(e)
-                    break
-
-    pickle.dump(sizes, open("../report/data/sizes_closed_infogain.p", "wb"))
+    aux(algs, names, 'closed_infogain')
--- project2/src/benchmark_time.py
+++ project2/src/benchmark_time.py
@@ -12,6 +12,37 @@ import matplotlib.pyplot as plt
 
 plt.style.use("ggplot")
 
+def aux(algs, names, file):
+    datasets = ['Protein', 'Reuters']
+    pos_files = ['SRC1521', 'earn']
+    neg_files = ['PKA_group15', 'acq']
+
+    times = {}
+    for data in datasets:
+        times[data] = {}
+        for alg in names:
+            times[data][alg] = []
+
+    for j, data in enumerate(datasets):
+        for i in range(len(algs)):
+            print()
+            alg = algs[i]
+            k = 1
+            while True:
+                try:
+                    print("data: %s, alg: %s, k: %g" % (data, names[i], k))
+                    with timeout(seconds=200):
+                        s = time.perf_counter()
+                        alg("%s/%s.txt" % (data, pos_files[j]), "%s/%s.txt" % (data, neg_files[j]), k, False)
+                        e = time.perf_counter()
+                    times[data][names[i]].append(e-s)
+                    k *= 2
+                except (TimeoutError, RecursionError) as e:
+                    print(e)
+                    break
+
+    pickle.dump(times, open("../report/data/times_%s.p" % (file), "wb"))
+
 
 if __name__ == "__main__":
     import sumsup.PrefixSpan_sumsup_heap as ps_heap
@@ -19,174 +50,28 @@ if __name__ == "__main__":
     import sumsup.SPADE as spade
     algs = [file.main for file in [ps_heap, ps_sl, spade]]
     names = ['PrefixSpan_heap', 'PrefixSpan_sorted_list', 'SPADE']
-    datasets = ['Protein', 'Reuters']
-    pos_files = ['SRC1521', 'earn']
-    neg_files = ['PKA_group15', 'acq']
-
-    times = {}
-    for data in datasets:
-        times[data] = {}
-        for alg in names:
-            times[data][alg] = []
-
-    for j, data in enumerate(datasets):
-        for i in range(len(algs)):
-            print()
-            alg = algs[i]
-            k = 1
-            while True:
-                try:
-                    print("data: %s, alg: %s, k: %g" % (data, names[i], k))
-                    with timeout(seconds=200):
-                        s = time.perf_counter()
-                        alg("%s/%s.txt" % (data, pos_files[j]), "%s/%s.txt" % (data, neg_files[j]), k, False)
-                        e = time.perf_counter()
-                    times[data][names[i]].append(e-s)
-                    k *= 2
-                except TimeoutError as e:
-                    print(e)
-                    break
-
-    pickle.dump(times, open("../report/data/times_sumsup.p", "wb"))
-
+    aux(algs, names, 'sumsup')
 
     import wracc.PrefixSpan_wracc_heap as ps_heap
     import wracc.PrefixSpan_wracc_sorted_list as ps_sl
     algs = [file.main for file in [ps_heap, ps_sl]]
     names = ['PrefixSpan_heap', 'PrefixSpan_sorted_list']
-    datasets = ['Protein', 'Reuters']
-    pos_files = ['SRC1521', 'earn']
-    neg_files = ['PKA_group15', 'acq']
-
-    times = {}
-    for data in datasets:
-        times[data] = {}
-        for alg in names:
-            times[data][alg] = []
-
-    for j, data in enumerate(datasets):
-        for i in range(len(algs)):
-            print()
-            alg = algs[i]
-            k = 1
-            while True:
-                try:
-                    print("data: %s, alg: %s, k: %g" % (data, names[i], k))
-                    with timeout(seconds=200):
-                        s = time.perf_counter()
-                        alg("%s/%s.txt" % (data, pos_files[j]), "%s/%s.txt" % (data, neg_files[j]), k, False)
-                        e = time.perf_counter()
-                    times[data][names[i]].append(e-s)
-                    k *= 2
-                except TimeoutError as e:
-                    print(e)
-                    break
-
-    pickle.dump(times, open("../report/data/times_wracc.p", "wb"))
-
+    aux(algs, names, 'wracc')
 
     import closed_wracc.CloSpan_wracc_heap as cs_heap
     import closed_wracc.CloSpan_wracc_sorted_list as cs_sl
     algs = [file.main for file in [cs_heap, cs_sl]]
     names = ['CloSpan_heap', 'CloSpan_sorted_list']
-    datasets = ['Protein', 'Reuters']
-    pos_files = ['SRC1521', 'earn']
-    neg_files = ['PKA_group15', 'acq']
-
-    times = {}
-    for data in datasets:
-        times[data] = {}
-        for alg in names:
-            times[data][alg] = []
-
-    for j, data in enumerate(datasets):
-        for i in range(len(algs)):
-            print()
-            alg = algs[i]
-            k = 1
-            while True:
-                try:
-                    print("data: %s, alg: %s, k: %g" % (data, names[i], k))
-                    with timeout(seconds=200):
-                        s = time.perf_counter()
-                        alg("%s/%s.txt" % (data, pos_files[j]), "%s/%s.txt" % (data, neg_files[j]), k, False)
-                        e = time.perf_counter()
-                    times[data][names[i]].append(e-s)
-                    k *= 2
-                except TimeoutError as e:
-                    print(e)
-                    break
-
-    pickle.dump(times, open("../report/data/times_closed_wracc.p", "wb"))
-
+    aux(algs, names, 'closed_wracc')
 
     import closed_abswracc.CloSpan_abswracc_heap as cs_heap
     import closed_abswracc.CloSpan_abswracc_sorted_list as cs_sl
     algs = [file.main for file in [cs_heap, cs_sl]]
     names = ['CloSpan_heap', 'CloSpan_sorted_list']
-    datasets = ['Protein', 'Reuters']
-    pos_files = ['SRC1521', 'earn']
-    neg_files = ['PKA_group15', 'acq']
-
-    times = {}
-    for data in datasets:
-        times[data] = {}
-        for alg in names:
-            times[data][alg] = []
-
-    for j, data in enumerate(datasets):
-        for i in range(len(algs)):
-            print()
-            alg = algs[i]
-            k = 1
-            while True:
-                try:
-                    print("data: %s, alg: %s, k: %g" % (data, names[i], k))
-                    with timeout(seconds=200):
-                        s = time.perf_counter()
-                        alg("%s/%s.txt" % (data, pos_files[j]), "%s/%s.txt" % (data, neg_files[j]), k, False)
-                        e = time.perf_counter()
-                    times[data][names[i]].append(e-s)
-                    k *= 2
-                except TimeoutError as e:
-                    print(e)
-                    break
-
-    pickle.dump(times, open("../report/data/times_closed_abswracc.p", "wb"))
-
+    aux(algs, names, 'closed_abswracc')
 
     import closed_infogain.CloSpan_infogain_heap as cs_heap
     import closed_infogain.CloSpan_infogain_sorted_list as cs_sl
     algs = [file.main for file in [cs_heap, cs_sl]]
     names = ['CloSpan_heap', 'CloSpan_sorted_list']
-    datasets = ['Protein', 'Reuters']
-    pos_files = ['SRC1521', 'earn']
-    neg_files = ['PKA_group15', 'acq']
-
-    times = {}
-    for data in datasets:
-        times[data] = {}
-        for alg in names:
-            times[data][alg] = []
-
-    for j, data in enumerate(datasets):
-        for i in range(len(algs)):
-            print()
-            alg = algs[i]
-            k = 1
-            while True:
-                try:
-                    print("data: %s, alg: %s, k: %g" % (data, names[i], k))
-                    with timeout(seconds=200):
-                        s = time.perf_counter()
-                        alg("%s/%s.txt" % (data, pos_files[j]), "%s/%s.txt" % (data, neg_files[j]), k, False)
-                        e = time.perf_counter()
-                    times[data][names[i]].append(e-s)
-                    k *= 2
-                except TimeoutError as e:
-                    print(e)
-                    break
-
-    pickle.dump(times, open("../report/data/times_closed_infogain.p", "wb"))
-
-
+    aux(algs, names, 'closed_infogain')
